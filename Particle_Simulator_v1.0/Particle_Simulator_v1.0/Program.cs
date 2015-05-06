5dac47647c722c3603b495d49d536d429525319f
yunzhe <ericliyunzhe@gmail.com>
2015-05-06 21:57:42 +0800

    Second commit to add all necessary files initialsed by MS VS 2010

---
+++ Particle_Simulator_v1.0/Particle_Simulator_v1.0/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Particle_Simulator_v1._0
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
